22f0cefdc2f9bf5d80d909413dd089ed98f091b0
Brandon Perry <bperry.volatile@gmail.com>
2015-09-25 18:57:54 -0500

    Update to deal with strings

--- metasploit-sharp/MetasploitSession.cs
+++ metasploit-sharp/MetasploitSession.cs
@@ -63,8 +63,14 @@ namespace metasploitsharp
 			request.ContentType = "binary/message-pack";
 			request.Method = "POST";
 			request.KeepAlive = true;
-			
-			Stream requestStream = request.GetRequestStream ();
+
+			Stream requestStream = null;
+
+			try{
+				requestStream = request.GetRequestStream ();
+			} catch (Exception ex) {
+				Console.WriteLine (ex);
+			}
 
 			Packer msgpackWriter = Packer.Create(requestStream);
 			
@@ -132,48 +138,61 @@ namespace metasploitsharp
 				if (obj.UnderlyingType == null)
 					continue;
 				
-				if (obj.IsRaw)
-				{
-					if (obj.IsTypeOf (typeof(string)).Value)
-						returnDictionary [pair.Key.ToString ()] = obj.ToString ();
+				if (obj.IsRaw) {
+					if (obj.UnderlyingType == typeof(string))
+					if (pair.Key.IsRaw && pair.Key.IsTypeOf (typeof(Byte[])).Value)
+						returnDictionary [System.Text.Encoding.ASCII.GetString ((byte[])pair.Key)] = obj.AsString ();
+					else
+						returnDictionary [pair.Key.ToString ()] = obj.AsString ();
 					else if (obj.IsTypeOf (typeof(int)).Value)
 						returnDictionary [pair.Key.ToString ()] = (int)obj.ToObject ();
-					else if (obj.IsTypeOf (typeof(Byte[])).Value)
-						returnDictionary [pair.Key.ToString ()] = (Byte[])obj.ToObject ();
-					else 
-						throw new Exception("I don't know type: " + pair.Value.GetType().Name);
-				}
-				else if (obj.IsArray)
-				{
-					List<object> arr = new List<object>();
-					foreach (var o in obj.ToObject() as MessagePackObject[])
-					{
+					else if (obj.IsTypeOf (typeof(Byte[])).Value) {
+						returnDictionary [System.Text.Encoding.ASCII.GetString ((Byte[])pair.Key)] = System.Text.Encoding.ASCII.GetString ((Byte[])obj.ToObject ());
+					} else
+						throw new Exception ("I don't know type: " + pair.Value.GetType ().Name);
+				} else if (obj.IsArray) {
+					List<object> arr = new List<object> ();
+					foreach (var o in obj.ToObject() as MessagePackObject[]) {
 						if (o.IsDictionary)
-							arr.Add(TypifyDictionary(o.AsDictionary()));
+							arr.Add (TypifyDictionary (o.AsDictionary ()));
 						else if (o.IsRaw)
-							arr.Add(o.ToString());
-						else if (o.IsArray)
-						{
-							var enu = o.AsEnumerable();
-							List<object> array = new List<object>();
+							arr.Add (System.Text.Encoding.ASCII.GetString ((byte[])o));
+						else if (o.IsArray) {
+							var enu = o.AsEnumerable ();
+							List<object> array = new List<object> ();
 							foreach (var blah in enu)
-								array.Add(blah as object);
+								array.Add (blah as object);
 
-							arr.Add(array.ToArray());
-						}else if (o.ToObject().GetType() == typeof(Byte)) //this is a hack because I don't know what type you are...
-							arr.Add(o.ToString());
+							arr.Add (array.ToArray ());
+						} else if (o.ToObject ().GetType () == typeof(Byte)) //this is a hack because I don't know what type you are...
+							arr.Add (o.ToString ());
 					}
-					
-					returnDictionary.Add(pair.Key.AsString(), arr);
+
+					if (pair.Key.IsRaw && pair.Key.IsTypeOf (typeof(Byte[])).Value)
+						returnDictionary.Add (System.Text.Encoding.ASCII.GetString ((byte[])pair.Key), arr);
+					else
+						returnDictionary.Add (pair.Key.AsString (), arr);
+				} else if (obj.IsDictionary) {
+					if (pair.Key.IsRaw && pair.Key.IsTypeOf(typeof(Byte[])).Value)
+						returnDictionary [System.Text.Encoding.ASCII.GetString((byte[])pair.Key)] = TypifyDictionary (obj.AsDictionary ());
+					else 
+						returnDictionary [pair.Key.ToString ()] = TypifyDictionary (obj.AsDictionary ());
+				} else if (obj.IsTypeOf (typeof(UInt16)).Value) {
+					if (pair.Key.IsRaw && pair.Key.IsTypeOf (typeof(Byte[])).Value)
+						returnDictionary [System.Text.Encoding.ASCII.GetString ((byte[])pair.Key)] = obj.AsUInt16 ();
+					else
+						returnDictionary [pair.Key.ToString ()] = obj.AsUInt16 ();
+				} else if (obj.IsTypeOf (typeof(UInt32)).Value) {
+					if (pair.Key.IsRaw && pair.Key.IsTypeOf (typeof(Byte[])).Value)
+						returnDictionary [System.Text.Encoding.ASCII.GetString ((byte[])pair.Key)] = obj.AsUInt32 ();
+					else
+						returnDictionary [pair.Key.ToString ()] = obj.AsUInt32 ();
+				} else if (obj.IsTypeOf (typeof(bool)).Value) {
+					if (pair.Key.IsRaw && pair.Key.IsTypeOf (typeof(Byte[])).Value)
+						returnDictionary [System.Text.Encoding.ASCII.GetString ((byte[])pair.Key)] = obj.AsBoolean ();
+					else
+						returnDictionary [pair.Key.ToString ()] = obj.AsBoolean ();
 				}
-				else if (obj.IsDictionary)
-					returnDictionary[pair.Key.ToString()] = TypifyDictionary(obj.AsDictionary());
-				else if (obj.IsTypeOf(typeof(UInt16)).Value)
-					returnDictionary[pair.Key.ToString()] = obj.AsUInt16();
-				else if (obj.IsTypeOf(typeof(UInt32)).Value)
-					returnDictionary[pair.Key.ToString()] = obj.AsUInt32();
-				else if (obj.IsTypeOf(typeof(bool)).Value)
-					returnDictionary[pair.Key.ToString()] = obj.AsBoolean();
 				else 
 					throw new Exception("Don't know type: " + obj.ToObject().GetType().Name);
 			}
